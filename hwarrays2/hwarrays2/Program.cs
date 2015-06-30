b8d5efacc015536a58380764725aa9fad510c91b
rkirilova <r.s.kirilova@gmail.com>
2015-06-30 22:45:10 +0300

    hwarrays2

---
+++ hwarrays2/hwarrays2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwarrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arry = new int[,]{
+                {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12}
+            };
+            for (int i = 0; i < arry.GetLength(0); i++)
+            {
+                for (int j = 0; j < arry.GetLength(1); j++)
+                {
+                    if (arry[i, j] % 2 == 0)
+                    {
+                        Console.WriteLine(arry[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ hwarrays2/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arry = new int[3, 4];
+
+            for (int i = 0; i < arry.GetLength(0); i++)
+            {
+                for (int j = 0; j < arry.GetLength(1); j++)
+                {
+                    arry[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+
+            for (int i = 0; i < arry.GetLength(0); i++)
+            {
+                for (int j = 0; j < arry.GetLength(1); j++)
+                {
+                    Console.Write("   " + arry[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
